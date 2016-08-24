1d629342402f61121a0b0077c84e58e0badc8dc5
sanky.avi@gmail.com <377304@CTS.COM>
2016-08-24 14:47:26 +0530

    finished 4.multiple components

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   //We're passing metadata to the NgModule decorator function:
   imports:      [ BrowserModule,FormsModule ],  //imports - the other modules that export material we need in this module.
-  declarations: [ AppComponent ],   //components and directives that belong to this module.
+  declarations: [ AppComponent, HeroDetailComponent ],   //components and directives that belong to this module.
   bootstrap:    [ AppComponent ]    // identifies the root component that Angular should bootstrap when it starts the application
 })
 export class AppModule { }

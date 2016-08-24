60ab30aa789bb21314fb7f0ad73ff1f9768b64c1
sanky.avi@gmail.com <377304@CTS.COM>
2016-08-24 14:08:05 +0530

    finished master detail

--- app/app.component.ts
+++ app/app.component.ts
@@ -30,16 +30,31 @@ const HEROES: Hero[] = [
   <h1>{{title}}</h1>
   <h2>My Heroes</h2>
   <ul class="heroes">
-  <li *ngFor="let hero of heroes">
+  <li *ngFor="let hero of heroes"
+    [class.selected]="hero === selectedHero"
+    (click)="onselect(hero)">
     <span class="badge">{{hero.id}}</span> {{hero.name}}
   </li>
   </ul>
+  <div *ngIf="selectedHero">
+  <h2>{{selectedHero.name}} details!</h2>
+  <div><label>id: </label>{{selectedHero.id}}</div>
+  <div>
+    <label>name: </label>
+    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+  </div>
+  </div>
   `
 })
 //We export AppComponent so that we can import it elsewhere in our application
 export class AppComponent {
   title : string =  "tour of heroes";
   heroes =  HEROES;
+  selectedHero : Hero;
+
+  onselect (hero:Hero): void {
+    this.selectedHero = hero;
+  }
  }   // AppComponent is the root of the application
 
 
